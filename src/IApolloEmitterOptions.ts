2a5b125fc8b2315dad78d8474ede6a41396e0fd5
Daniel Gary <daniel@gatewayapps.com>
2019-05-24 08:04:07 -0500

    removed model reference reference

--- src/IApolloEmitterOptions.ts
+++ src/IApolloEmitterOptions.ts
@@ -1,5 +1,4 @@
 import { CradleModel, ICradleOperation } from '@gatewayapps/cradle'
-import ModelReference from '@gatewayapps/cradle/dist/lib/ModelReference'
 import PropertyType from '@gatewayapps/cradle/dist/lib/PropertyTypes/PropertyType'
 
 export interface IApolloEmitterOptions {
@@ -16,7 +15,6 @@ export interface IApolloEmitterOptions {
   readonly shouldOutputResolverFiles: boolean
   readonly shouldGenerateResolvers?: (model: CradleModel) => boolean
   readonly shouldTypeIncludeProperty?: (model: CradleModel, propertyName: string, property: PropertyType) => boolean
-  readonly shouldTypeIncludeReference?: (model: CradleModel, referenceName: string, modelReference: ModelReference) => boolean
   readonly shouldTypeIncludeOperation?: (model: CradleModel, operationName: string, operation: ICradleOperation) => boolean
   readonly getDirectiveForResolver?: (model: CradleModel, resolverType: string, resolverName: string) => string
   readonly onComplete: (filesEmitted: string[]) => void
